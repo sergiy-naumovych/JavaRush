7881fb933b6a5e19f1280f0d7442626f9ea3b263
Serge009 <serge.naumovych@gmail.com>
2014-06-30 18:11:17 +0300

    solved

--- src/com/javarush/test/level05/lesson12/home04/Solution.java
+++ src/com/javarush/test/level05/lesson12/home04/Solution.java
@@ -4,17 +4,15 @@ package com.javarush.test.level05.lesson12.home04;
 Вывести на экран текущую дату в аналогичном виде "21 12 2012".
 */
 
-import java.util.Calendar;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Solution
 {
     public static void main(String[] args)
     {
-        Date t = new Date();
-        System.out.println(t);
-        Calendar today = Calendar.getInstance();
-
-        System.out.print(today.get(Calendar.DATE) + " " + (today.get(Calendar.MONTH) + 1) + " " + today.get(Calendar.YEAR));
+        Date d = new Date();
+        SimpleDateFormat f = new SimpleDateFormat("dd MM yyy");
+        System.out.println(f.format(d));
     }
 }
