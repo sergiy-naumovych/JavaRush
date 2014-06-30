d7b6ba514e66d3e4afebaf29bf12b4f22d894e2a
Serge009 <serge.naumovych@gmail.com>
2014-06-30 18:02:10 +0300

    solved

--- src/com/javarush/test/level03/lesson04/task01/Solution.java
+++ src/com/javarush/test/level03/lesson04/task01/Solution.java
@@ -4,17 +4,12 @@ package com.javarush.test.level03.lesson04.task01;
 Вывести на экран дату своего рождения в виде: MAY 1 2012
 */
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
+
 
 public class Solution
 {
     public static void main(String[] args)
     {
-        Date date = new Date("09/13/1991");
-
-        System.out.println(date.toLocaleString());
-
+        System.out.println("MAY 1 2012");
     }
 }
