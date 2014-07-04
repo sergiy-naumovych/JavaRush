9fe5647f834cb5e4c9ab4300d675a0e012ca48c4
Serge009 <serge.naumovych@gmail.com>
2014-07-04 14:29:48 +0300

    solved

---
+++ src/com/javarush/test/level18/lesson10/home08/Solution.java
@@ -0,0 +1,78 @@
+package com.javarush.test.level18.lesson10.home08;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+
+/* Нити и байты
+Читайте с консоли имена файлов, пока не будет введено слово "exit"
+Передайте имя файла в нить ReadThread
+Нить ReadThread должна найти байт, который встречается в файле максимальное число раз, и добавить его в словарь resultMap,
+где параметр String - это имя файла, параметр Integer - это искомый байт.
+Не забудьте закрыть все потоки
+*/
+
+public class Solution {
+    public static Map<String, Integer> resultMap = new HashMap<String, Integer>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName;
+        while(!(fileName = reader.readLine()).equals("exit")){
+            System.out.println(fileName);
+            new ReadThread(fileName).start();
+        }
+
+        for(Map.Entry<String, Integer> entry : resultMap.entrySet()){
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        }
+
+        reader.close();
+    }
+
+    public static class ReadThread extends Thread {
+        private String fileName;
+
+        public ReadThread(String fileName) {
+            this.fileName = fileName;
+        }
+
+        @Override
+        public void run() {
+            HashMap<Byte, Integer> map = new HashMap<>();
+            byte[] data = new byte[0];
+            int max = Integer.MIN_VALUE;
+            byte res = Byte.MIN_VALUE;
+
+            try(FileInputStream in = new FileInputStream(this.fileName)) {
+                while(in.available() > 0){
+                    data = new byte[in.available()];
+                    in.read(data);
+                }
+            } catch (IOException e) {
+                //NOP
+            }
+
+            for(byte b : data){
+                if(map.containsKey(b)){
+                    int count = map.get(b) + 1;
+                    map.put(b, count);
+                } else {
+                    map.put(b, 1);
+                }
+            }
+
+            for(Map.Entry<Byte, Integer> entry : map.entrySet()){
+                if(entry.getValue() > max){
+                    max = entry.getValue();
+                    res = entry.getKey();
+                }
+            }
+
+            synchronized (resultMap){
+                resultMap.put(this.fileName, (int)res);
+            }
+        }
+
+    }
+}
